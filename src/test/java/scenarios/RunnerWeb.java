74b16c0315f62e3d687c814c0b77b60c901cc221
Flávio Dias <flavio.goncalvesdias@gmail.comm>
2020-09-30 00:04:29 -0300

    Adicionando chomedrive para windows e Surefire plugin

--- src/test/java/scenarios/RunnerWeb.java
+++ src/test/java/scenarios/RunnerWeb.java
@@ -6,7 +6,8 @@ import io.cucumber.junit.CucumberOptions;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features",monochrome = false, glue = { "scenarios"}, tags={"@testeTabelas"})
+@CucumberOptions(features = "src/test/resources/features",monochrome = false, glue = 
+{ "scenarios"})
 public class RunnerWeb {
 
 }
